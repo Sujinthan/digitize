d47ecb82e6c853fcd55005b8b1a54a24894981c1
Sujinthan <s.sujinthan@gmail.com>
2020-11-03 21:39:18 -0500

    prevent scrolling when touching

--- static/js/script.js
+++ static/js/script.js
@@ -54,6 +54,22 @@ function init() {
             y: touchEvent.touches[0].clientY - rect.top
         };
     }
+    // Prevent scrolling when touching the canvas
+    document.body.addEventListener("touchstart", function (e) {
+        if (e.target == canvas) {
+            e.preventDefault();
+        }
+    }, false);
+    document.body.addEventListener("touchend", function (e) {
+        if (e.target == canvas) {
+            e.preventDefault();
+        }
+    }, false);
+    document.body.addEventListener("touchmove", function (e) {
+        if (e.target == canvas) {
+            e.preventDefault();
+        }
+    }, false);
 }
 
 function Draw(x, y, isDown) {
